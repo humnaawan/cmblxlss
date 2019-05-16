ecba6baaad2d7c94747161b9c0ec04c1f376d6ac
Humna Awan <humna.awan@rutgers.edu>
2019-05-16 18:41:26 -0400

    Clean up + updated labels + updated plotting routine usage.

--- code/get_spectra.py
+++ code/get_spectra.py
@@ -115,48 +115,44 @@ ells = np.arange(0, lmax)
 # calculate correlations
 readme = print_update(update='## Calculating correlations ... \n',
                       readme=readme)
-# autos
-#c_ells[r'$\kappa\kappa$ baseline'] = hp.alm2cl(kappa_alms_normed)[0:lmax]
+# auto kappa kappa
+c_ells[r'$\kappa\kappa$'] = hp.alm2cl(kappa_alms_normed)[0:lmax]
 c_ells[r'$\kappa\kappa$ w/ fg'] = hp.alm2cl(kappa_alms_normed_fg)[0:lmax]
-c_ells[r'$\kappa\kappa$ w/ lsst mask'] = hp.alm2cl(kappa_alms_normed_masked)[0:lmax]/lsst_fsky
-c_ells[r'$\kappa\kappa$ w/ fg + lsst mask'] = hp.alm2cl(kappa_alms_normed_fg_masked)[0:lmax]/lsst_fsky
-# cross correlation
-#c_ells[r'$\kappa$ baseline x correlated $g$'] = hp.alm2cl(kappa_alms_normed, gal_density_alm)[0:lmax]
-#c_ells[r'$\kappa$ baseline x lsst modulated correlated $g$'] = hp.alm2cl(kappa_alms_normed, gal_density_alm_mod)[0:lmax]
-#c_ells[r'$\kappa$ w/ lsst mask x correlated $g$'] = hp.alm2cl(kappa_alms_normed_masked, gal_density_alm)[0:lmax]/lsst_fsky
+c_ells[r'$\kappa\kappa$ w/ lsst mask'] = hp.alm2cl(kappa_alms_normed_masked)[0:lmax]
+c_ells[r'$\kappa\kappa$ w/ fg + lsst mask'] = hp.alm2cl(kappa_alms_normed_fg_masked)[0:lmax]
+
+# cross correlation: correlate stuff s.t. both maps have the lsst mask
 c_ells[r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask'] = hp.alm2cl(kappa_alms_normed_masked, gal_density_alm_masked)[0:lmax]
-c_ells[r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask + modulation'] = hp.alm2cl(kappa_alms_normed_masked, gal_density_alm_mod_masked_nodust)[0:lmax]
+c_ells[r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask + modulation w/o dust'] = hp.alm2cl(kappa_alms_normed_masked, gal_density_alm_mod_masked_nodust)[0:lmax]
 c_ells[r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask + modulation w/ dust'] = hp.alm2cl(kappa_alms_normed_masked, gal_density_alm_mod_masked_wdust)[0:lmax]
-c_ells[r'$\kappa$ w/ lsst mask + fg x $g$ w/ lsst mask + modulation'] = hp.alm2cl(kappa_alms_normed_fg_masked, gal_density_alm_mod_masked_nodust)[0:lmax]
-#c_ells[r'$\kappa$ w/ fg x correlated $g$'] = hp.alm2cl(kappa_alms_normed_fg, gal_density_alm)[0:lmax]
-#c_ells[r'$\kappa$ w/ fg + lsst mask x correlated $g$'] = hp.alm2cl(kappa_alms_normed_fg_masked, gal_density_alm)[0:lmax]/lsst_fsky
+c_ells[r'$\kappa$ w/ lsst mask + fg x $g$ w/ lsst mask + modulation w/o dust'] = hp.alm2cl(kappa_alms_normed_fg_masked, gal_density_alm_mod_masked_nodust)[0:lmax]
 c_ells[r'$\kappa$ w/ lsst mask + fg x $g$ w/ lsst mask + modulation w/ dust'] = hp.alm2cl(kappa_alms_normed_fg_masked, gal_density_alm_mod_masked_wdust)[0:lmax]
 # -----------------------------------------------
 cls_dir = '%s/cls_dir/'%outdir
 if not os.path.exists(cls_dir): os.makedirs(cls_dir)
-
+# nominal bin width
+bin_width = 50
 # plot the cross-correlations in sets
+# -----------------------------------------------
 readme = print_update(update='## Plotting spectra + saving them in %s ... \n' % (cls_dir.split(outdir)[-1]),
                       readme=readme)
-bin_width = 50
-# first plot the auto spectra for checks
+# -----------------------------------------------
+# first plot all the kappa kappa auto corrs
 c_ells_to_plot = {}
 for key in [f for f in c_ells.keys() if f.__contains__(r'\kappa\kappa')]:
     c_ells_to_plot[key] = c_ells[key]
 # plot
 filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='kk-only',
                          save_plot=True, show_plot=False,
-                         cross_convention=True,
-                         sci_yticks=True,
-                         binned=False, bin_width=bin_width, lmax=lmax)
+                         ylog=True, xlog=False, lmin=None, lmax=lmax,
+                         binned=False, bin_width=bin_width)
 readme = print_update(update='Saved %s'%filename,
                       readme=readme)
 # plot binned
 filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='kk-only',
                          save_plot=True, show_plot=False,
-                         cross_convention=True,
-                         sci_yticks=True,
-                         binned=True, bin_width=bin_width, lmax=lmax)
+                         ylog=True, xlog=False, lmin=None, lmax=lmax,
+                         binned=True, bin_width=bin_width)
 readme = print_update(update='Saved %s'%filename,
                       readme=readme)
 # -----------------------------------------------
@@ -172,38 +168,38 @@ linestyles = ['-', '-', '--', '--', '-']
 # plot
 filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='kg-only',
                          save_plot=True, show_plot=False,
-                         cross_convention=True,
-                         sci_yticks=True, colors=colors, markers=markers, linestyles=linestyles,
-                         binned=False, bin_width=bin_width, lmax=lmax)
+                         ylog=True, xlog=False, lmin=None, lmax=lmax,
+                         colors=colors, markers=markers, linestyles=linestyles,
+                         binned=False, bin_width=bin_width)
 readme = print_update(update='Saved %s'%filename,
                       readme=readme)
 # binned
 filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='kg-only',
                          save_plot=True, show_plot=False,
-                         cross_convention=True,
-                         sci_yticks=True, colors=colors, markers=markers, linestyles=linestyles,
-                         binned=True, bin_width=bin_width, lmax=lmax)
+                         ylog=True, xlog=False, lmin=None, lmax=lmax,
+                         colors=colors, markers=markers, linestyles=linestyles,
+                         binned=True, bin_width=bin_width)
 readme = print_update(update='Saved %s'%filename,
                       readme=readme)
 # binned: residuals
 filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='kg-only',
                          save_plot=True, show_plot=False,
-                         cross_convention=True, colors=colors, markers=markers, linestyles=linestyles,
+                         colors=colors, markers=markers, linestyles=linestyles,
                          residuals=True, baseline_key=r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask',
-                         sci_yticks=True, loglog=False,
-                         binned=True, bin_width=bin_width, lmax=lmax)
+                         ylog=False, xlog=True, lmin=None, lmax=lmax,
+                         binned=True, bin_width=bin_width)
 readme = print_update(update='Saved %s'%filename,
                       readme=readme)
 # binned: residuals: zoomed: all
 filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='kg-only',
                          save_plot=True, show_plot=False,
-                         cross_convention=True, colors=colors, markers=markers, linestyles=linestyles,
+                         colors=colors, markers=markers, linestyles=linestyles,
                          residuals=True, baseline_key=r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask',
-                         sci_yticks=True, loglog=True,
-                         binned=True, bin_width=30, lmax=lmax, lmin=10)
+                         ylog=False, xlog=True, lmin=10, lmax=lmax,
+                         binned=True, bin_width=30)
 readme = print_update(update='Saved %s'%filename,
                       readme=readme)
-
+# ----------------------------------
 # plot a subset
 # set up colors, markers
 markers = ['+', '+', 'x']
@@ -212,7 +208,7 @@ linestyles = ['-', '-', '-']
 
 c_ells_to_plot = {}
 keys = [r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask',
-        r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask + modulation',
+        r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask + modulation w/o dust',
         r'$\kappa$ w/ lsst mask + fg x $g$ w/ lsst mask + modulation w/ dust']
 for key in keys:
     c_ells_to_plot[key] = c_ells[key]
@@ -220,19 +216,19 @@ for key in keys:
 # binned
 filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='kg-only',
                          save_plot=True, show_plot=False,
-                         cross_convention=True,
-                         sci_yticks=True, colors=colors, markers=markers, linestyles=linestyles,
-                         binned=True, bin_width=bin_width, lmax=lmax)
+                         ylog=True, xlog=True, lmin=None, lmax=lmax,
+                         colors=colors, markers=markers, linestyles=linestyles,
+                         binned=True, bin_width=bin_width)
 readme = print_update(update='Saved %s'%filename,
                       readme=readme)
 
 # binned: residuals: zoomed: some
 filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='kg-only',
                          save_plot=True, show_plot=False,
-                         cross_convention=True, colors=colors, markers=markers, linestyles=linestyles,
+                         colors=colors, markers=markers, linestyles=linestyles,
                          residuals=True, baseline_key=r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask',
-                         sci_yticks=True, loglog=True,
-                         binned=True, bin_width=30, lmax=lmax, lmin=10)
+                         ylog=False, xlog=True, lmin=10, lmax=lmax,
+                         binned=True, bin_width=30)
 readme = print_update(update='Saved %s'%filename,
                       readme=readme)
 # -----------------------------------------------
@@ -248,3 +244,4 @@ readme = print_update(update='\nTime taken for the whole thing: %.3f min\n' % ((
 readme_file = open('%s/readme_%s.txt' % (outdir, readme_tag), 'a')
 readme_file.write(readme)
 readme_file.close()
+
