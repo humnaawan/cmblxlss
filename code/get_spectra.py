b4d8491e47bbc03929620bad4f18e4cdbe8f49e2
Humna Awan <humna.awan@rutgers.edu>
2019-05-16 18:42:07 -0400

    Added code to plot gg correlations.

--- code/get_spectra.py
+++ code/get_spectra.py
@@ -115,11 +115,20 @@ ells = np.arange(0, lmax)
 # calculate correlations
 readme = print_update(update='## Calculating correlations ... \n',
                       readme=readme)
-# auto kappa kappa
+# autos
+# kappa kappa
 c_ells[r'$\kappa\kappa$'] = hp.alm2cl(kappa_alms_normed)[0:lmax]
 c_ells[r'$\kappa\kappa$ w/ fg'] = hp.alm2cl(kappa_alms_normed_fg)[0:lmax]
 c_ells[r'$\kappa\kappa$ w/ lsst mask'] = hp.alm2cl(kappa_alms_normed_masked)[0:lmax]
 c_ells[r'$\kappa\kappa$ w/ fg + lsst mask'] = hp.alm2cl(kappa_alms_normed_fg_masked)[0:lmax]
+
+# gg
+c_ells[r'$gg$'] = hp.alm2cl(gal_density_alm)[0:lmax]
+c_ells[r'$gg$ w/ lsst mask'] = hp.alm2cl(gal_density_alm_masked)[0:lmax]
+c_ells[r'$gg$ w/ modulated w/ dust'] = hp.alm2cl(gal_density_alm_mod_wdust)[0:lmax]
+c_ells[r'$gg$ w/ modulated w/ dust + lsst mask'] = hp.alm2cl(gal_density_alm_mod_masked_wdust)[0:lmax]
+c_ells[r'$gg$ w/ modulated w/o dust'] = hp.alm2cl(gal_density_alm_mod_nodust)[0:lmax]
+c_ells[r'$gg$ w/ modulated w/o dust + lsst mask'] = hp.alm2cl(gal_density_alm_mod_masked_nodust)[0:lmax]
 
 # cross correlation: correlate stuff s.t. both maps have the lsst mask
 c_ells[r'$\kappa$ w/ lsst mask x $g$ w/ lsst mask'] = hp.alm2cl(kappa_alms_normed_masked, gal_density_alm_masked)[0:lmax]
@@ -156,7 +165,26 @@ filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='kk-onl
 readme = print_update(update='Saved %s'%filename,
                       readme=readme)
 # -----------------------------------------------
-# now plot the cross correlations
+# now plot all the gg auto corrs
+c_ells_to_plot = {}
+for key in [f for f in c_ells.keys() if f.__contains__(r'gg')]:
+    c_ells_to_plot[key] = c_ells[key]
+# plot
+filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='gg-only',
+                         save_plot=True, show_plot=False,
+                         ylog=True, xlog=False, lmin=None, lmax=lmax,
+                         binned=False, bin_width=bin_width)
+readme = print_update(update='Saved %s'%filename,
+                      readme=readme)
+# plot binned
+filename = plot_cls_dict(cls_in=c_ells_to_plot, outdir=cls_dir, file_tag='gg-only',
+                         save_plot=True, show_plot=False,
+                         ylog=True, xlog=False, lmin=None, lmax=lmax,
+                         binned=True, bin_width=bin_width)
+readme = print_update(update='Saved %s'%filename,
+                      readme=readme)
+# -----------------------------------------------
+# and now plot the cross correlations
 c_ells_to_plot = {}
 for key in [f for f in c_ells.keys() if f.__contains__(' x ')]:
     c_ells_to_plot[key] = c_ells[key]
@@ -244,4 +272,3 @@ readme = print_update(update='\nTime taken for the whole thing: %.3f min\n' % ((
 readme_file = open('%s/readme_%s.txt' % (outdir, readme_tag), 'a')
 readme_file.write(readme)
 readme_file.close()
-
